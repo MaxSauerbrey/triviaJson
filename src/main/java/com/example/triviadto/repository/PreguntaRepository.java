b25f2698c99593e0cb332c3c5a74d7bb9ead51b2
Max <maxsauerbrey@gmail.com>
2023-02-28 20:44:08 -0300

    fourth commit

--- src/main/java/com/example/triviadto/repository/PreguntaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.triviadto.repository;
-
-import com.example.triviadto.entity.Pregunta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PreguntaRepository extends JpaRepository<Pregunta, Long> {
-}
